b03a0b345d20cfcfe6f270814cdc74409446fcf7
suvinjosh <crsj777@gmail.com>
2023-12-11 12:03:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [name, setName]=useState("");
+  const [email, setEmail]=useState("");
+  const [password, setPassword]=useState("");
+  const [phone, setPhone]=useState("");
+  const [submitted, setSubmitted]=useState(false);
+  const [selectedSemester, setSelectedSemester]=useState('none');
+  const [selectedBranch, setSelectedBranch]=useState('none');
+
+  const handleSemesterChange = (e)=> {
+    setSelectedSemester(e.target.value);
+  };
+  const handleBranchChange = (e)=> {
+    setSelectedBranch(e.target.value);
+  };
+  const handleName = (e)=> {
+    setName(e.target.value);
+  };
+  const handleEmail = (e)=> {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e)=> {
+    setPassword(e.target.value);
+  };
+  const handlePhone = (e)=> {
+    setPhone(e.target.value);
+  };
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    if(name === "|| email ===" || password ==="|| phone"){
+      alert('Please enter all the fields');
+    } else {
+      setSubmitted(true);
+    }
+  };
+  const successMessage = () => {
+    if (submitted) {
+      return (
+
+        <form>
+          <h3>Student Profile</h3>
+          <p>Name:<span id ="pname">{name}</span></p>
+          <p>Email:<span id ="pemail">{email}</span></p>
+          <p>Phone:<span id ="pphone">{phone}</span></p>
+          <p>Semester:<span id ="psem">{selectedSemester}</span></p>
+          <p>Branch:<span id ="pbranch">{selectedBranch}</span></p>
+          
+          </form>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form">
+      {/* Calling to the methods */}
+      <div className="message">{successMessage()}</div>
+      {/* Conditionally render the form or success message */}
+      {!submitted && (
+        <form>
+          <div>
+            <hl>User Registration</hl>
+          </div>
+        
+        {/* Label and inputs for form data */}
+        <label className="label">Name</label>
+         <input onChange={handleName}className="input" value={name}type="text"/> <br></br>
+         <label className="label">Email</label>
+         <input onChange={handleEmail}className="input" value={email}type="email"/> <br></br>
+         <label className="label">Phone Number</label>
+         <input onChange={handlePhone}className="input" value={phone}type="number"/> <br></br>
+         <label htmlFor="sem">Semester</label>
+         <select id="sem"value={selectedSemester} onChange={handleSemesterChange}>
+          <option value="none" disabled>Select your semester
+            </option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            </select>
+            <br></br>
+            <label htmlFor="branch"></label>
+            <select id="branch" value={selectedBranch}>
+              <option value="none" disabled>Select your branch</option>
+              <option value="cs">CS</option>
+              <option value="ec">EC</option>
+              <option value="ee">EE</option>
+              <option value="mech">Mech</option>
+              <option value="civil">Civil</option>
+              </select>
+              <br></br>
+              <label className="label">Password</label>
+              <input onChange={handlePassword} className="input"value={password}type="password"/> <br></br>
+              <button onClick="btn" type="submit">Submit</button>
+              </form>
+      )}
     </div>
   );
 }
